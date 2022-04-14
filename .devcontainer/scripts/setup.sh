666a1ebdb536a133a31d9d9206f9b2904cef681e
anntesi5168 <b01303117@gmail.com>
2022-04-13 20:04:57 -0400

    add some annotation

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -2,5 +2,5 @@
 echo "Setting up Docker lab environment..."
 docker pull alpine
 docker pull python:3.9-slim
-docker run --restart=always -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
+docker run --restart=always -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine # mount redis on /data folder
 echo "Setup complete"
